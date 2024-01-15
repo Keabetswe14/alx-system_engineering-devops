e59865beb21e70f2f9f5e2857e1168a97f9f780b
Keabetswe14 <londiwedlamini857@gmail.com>
2024-01-15 00:39:30 -0800

    deletions

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,2 +0,0 @@
-# Changes OS configuration so that it is possible to login with holberton user and open file without any error message
-exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
